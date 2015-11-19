9c603a4a18fdfc2cbdbdbe3e24213980582b0235
jcapellman <jcapellman@hotmail.com>
2015-11-18 22:37:58 -0500

    Added: Console Test app

---
+++ jcIS.Tests.Console/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jcIS.Tests.Console {
+    class Program {
+        static void Main(string[] args) {
+
+        }
+    }
+}
