423cdb6ca70de7802d3dd3f2a684f23a0a24b5b8
Jarred Capellman <jcapellman@hotmail.com>
2016-05-23 21:29:14 -0400

    Added new WPF App

--- jcIS.PCL/jcScaler.cs
+++ jcIS.PCL/jcScaler.cs
@@ -2,11 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using NOpenCL;
 
 namespace jcIS.PCL
 {
     public class jcScaler
     {
-        +
+        public List<Platform> GetPlatforms() => Platform.GetPlatforms().ToList();
     }
 }
---
+++ jcIS.WPF/MainWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+using jcIS.PCL;
+using MahApps.Metro.Controls;
+
+namespace jcIS.WPF {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow {
+        public MainWindow() {
+            InitializeComponent();
+
+            var c = new jcScaler();
+
+            lvPlatforms.ItemsSource = c.GetPlatforms().Select(a => a.Name).ToList();
+        }
+    }
+}
