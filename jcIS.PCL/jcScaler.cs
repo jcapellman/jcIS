e2b1bf421c37052a3cc23742920423e8a7ab1abb
Jarred Capellman <jcapellman@hotmail.com>
2016-05-24 20:16:31 -0400

    Axdded NOpenCL

--- jcIS.PCL/jcScaler.cs
+++ jcIS.PCL/jcScaler.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using NOpenCL;
+using Buffer = NOpenCL.Buffer;
 
 namespace jcIS.PCL
 {
     public class jcScaler
     {
         public List<Platform> GetPlatforms() => Platform.GetPlatforms().ToList();
+
+        public void CreateBuffer(Platform platform)
+        {
+            using (Context context = Context.Create(platform.GetDevices())) {
+                using (Buffer buffer = context.CreateBuffer(MemoryFlags.AllocateHostPointer, 1024)) {
+                    
+                }
+            }
+        }
     }
 }
