ff15e3ae0d18b479a1793f3ae264d4e531276d13
Jarred Capellman <jcapellman@hotmail.com>
2016-05-25 21:48:04 -0400

    Added Settings Control

---
+++ jcIS.WPF/Controls/SettingsControl.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Controls;
+
+using jcIS.WPF.ViewModels;
+
+namespace jcIS.WPF.Controls {
+    public partial class SettingsControl : UserControl {
+        private SettingsModel viewModel => (SettingsModel) DataContext;
+
+        public SettingsControl() {
+            InitializeComponent();
+
+            DataContext = new SettingsModel();
+
+            viewModel.LoadSettings();
+        }
+    }
+}
---
+++ jcIS.WPF/ViewModels/SettingsModel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+using NOpenCL;
+
+namespace jcIS.WPF.ViewModels {
+    public class SettingsModel : INotifyPropertyChanged {
+        private List<Platform> _platforms;
+
+        public List<Platform> Platforms { get {  return _platforms;} set { _platforms = value; OnPropertyChanged(); } }
+
+        public void LoadSettings() {
+            Platforms = Platform.GetPlatforms().ToList();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
