fe5e7666905130f199b88deedc8fa2fbb3b04d2a
Jarred Capellman <jcapellman@hotmail.com>
2016-05-27 22:14:31 -0400

    Added Device and UI changes

--- jcIS.WPF/ViewModels/SettingsModel.cs
+++ jcIS.WPF/ViewModels/SettingsModel.cs
@@ -11,8 +11,22 @@ namespace jcIS.WPF.ViewModels {
 
         public List<Platform> Platforms { get {  return _platforms;} set { _platforms = value; OnPropertyChanged(); } }
 
+        private Platform _selectedPlatform;
+
+        public Platform SelectedPlatform { set { _selectedPlatform = value; OnPropertyChanged(); Devices = value.GetDevices().ToList(); } get { return _selectedPlatform; } }
+
+        private List<Device> _devices;
+
+        public List<Device> Devices {  get { return _devices; } set { _devices = value;  OnPropertyChanged(); SelectedDevice = value.FirstOrDefault(); } }
+
+        private Device _selectedDevice;
+
+        public Device SelectedDevice {  get { return _selectedDevice; } set { _selectedDevice = value;  OnPropertyChanged(); } }
+
         public void LoadSettings() {
             Platforms = Platform.GetPlatforms().ToList();
+
+            SelectedPlatform = Platforms.FirstOrDefault();
         }
 
         public event PropertyChangedEventHandler PropertyChanged;
